54702b155618aa12b6c2d4820d69d621e07f6e37
Moham7medAA الفارسي <Alfrsi-2007@hotmail.com>
2023-11-19 21:41:07 +0400

    Committing changes before updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms'; // Import this line
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule,NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
